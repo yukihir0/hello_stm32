bea34c98690627817198698b8e7b3c8dc5863c21
yukihir0 <yukihiro.cotori@gmail.com>
2020-09-25 11:00:15 +0900

    add user switch input

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ fn main() -> ! {
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
     
+    let gpioa = dp.GPIOA.split();
+    let user_sw = gpioa.pa0.into_pull_down_input();
+
     let gpiod = dp.GPIOD.split();
     let mut led_blue = gpiod.pd15.into_push_pull_output();
     let mut led_red = gpiod.pd14.into_push_pull_output();
@@ -24,16 +27,25 @@ fn main() -> ! {
     let mut delay = Delay::new(cp.SYST, clocks);
 
     for _ in 0..5 {
+      match user_sw.is_high() {
+        Ok(status)  => {
+          if status == true {
+            led_green.set_high().unwrap()
+          } else {
+            led_green.set_low().unwrap()
+          }
+        },
+        Err(_) => {},
+      }
+
       led_blue.set_high().unwrap();
       led_red.set_high().unwrap();
       led_orange.set_high().unwrap();
-      led_green.set_high().unwrap();
       delay.delay_ms(1000_u32);
 
       led_blue.set_low().unwrap();
       led_red.set_low().unwrap();
       led_orange.set_low().unwrap();
-      led_green.set_low().unwrap();
       delay.delay_ms(1000_u32);
     }
   }
